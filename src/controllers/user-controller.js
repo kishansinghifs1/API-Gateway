494b256f21709ee10e3b5f6fbb9af0bd18e02cd2
Srishruti24 <sri.shruti24@gmail.com>
2026-03-02 23:47:00 +0530

    Add user module

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,28 @@
+const {StatusCodes}=require('http-status-codes');
+const{UserService}=require('../services');
+
+const {ErrorResponse, SuccessResponse}=require('../utils/common');
+
+/*
+POST -> /signup 
+request body -> {email:'Shruti12@gmail.com',password:'1234'}
+ */
+async function signup(req, res){
+    try {
+        const user = await UserService.create({
+            email:req.body.email,
+            password:req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+        .status(StatusCodes.CREATED)
+        .json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res.status(error.statusCode)
+        .json(ErrorResponse);
+    }
+}
+module.exports={
+    signup
+}
---
+++ src/services/user-service.js
@@ -0,0 +1,22 @@
+const{UserRepository}=require('../repositories');
+const userRepo =new UserRepository();
+
+async function create(data){
+try {
+        const user = await userRepo.create(data);
+        return user;
+    } catch (error) {
+        if(error.name == 'SequelizeValidationError'||error.name == 'SequelizeUniqueConstraintError'){
+            let explanation = [];
+            error.errors.forEach((err)=>{
+                explanation.push(err.message);
+            });
+            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+        }
+        throw new AppError('Cannot create a user object', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+
+}
+module.exports={
+    create
+}
